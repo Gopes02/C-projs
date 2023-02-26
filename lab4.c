794fb9d3987c19bbe3edebfc0d26cadd6ad659ec
Gopal Walia <113048741+Gopes02@users.noreply.github.com>
2023-02-25 23:15:09 -0500

    finished part 1

--- lab4.c
+++ lab4.c
@@ -1,65 +1,71 @@
 #include <stdio.h>
 
 void string_input(char* str);
-void string_lenght(char* str, int* len);
+void string_length(char* str, int* len);
 int lower_case_test(char c);
 char lower_to_upper(char c);
 void capitalize_alpha_chars(char* str, int* strLen,int* count);
-void reverse_string(char* str, int* strLen);
-#define MAX_LENGHT 100
+void reverse_string(char* str, int* strLen, char* newStr );
+void print(char* newStr, int* strLen, int* count);
 
-int main(){
-    char str[MAX_LENGHT], lower[MAX_LENGHT];
+#define MAX_LENGTH 100
+
+int main() {
+    char str[MAX_LENGTH], lower[MAX_LENGTH], newStr[MAX_LENGTH];
     int strLen = 0, count = 0;
-    string_input(str);
-    string_lenght(str, &strLen);
-    capitalize_alpha_chars(str,&strLen,&count);
-    reverse_string(str, &strLen);
-
-}
-
-void string_input(char *str){
     
-    printf("Enter a string: ");
-    fgets(str,MAX_LENGHT,stdin);
+    string_input(str);
+    string_length(str, &strLen);
+    capitalize_alpha_chars(str,&strLen,&count);
+    reverse_string(str, &strLen, newStr);
+    print( newStr, &strLen, &count);
 
+    return 0;
 }
 
-void string_lenght(char* str, int* strLen){
-    int i =0; 
-    while( str[i] != '\0'){
+void string_input(char *str) {
+    printf("Enter a string: ");
+    fgets(str, MAX_LENGTH, stdin);
+}
+
+void string_length(char* str, int* strLen) {
+    int i = 0; 
+    while (str[i] != '\0') {
         i++;
     }
-    *strLen = i-1;
-    
+    *strLen = i - 1;
 }
 
-int lower_case_test(char c){
-    return (c >= 'a' && c<= 'z');
+int lower_case_test(char c) {
+    return (c >= 'a' && c <= 'z');
 }
 
-char lower_to_upper(char c){
-    return c = c - 'a' + 'A';
+char lower_to_upper(char c) {
+    return c - 'a' + 'A';
 }
 
-void capitalize_alpha_chars(char* str, int* strLen,int* count){
-    for (int i=0;i < *strLen;i++ ){
-        if(lower_case_test(str[i])){
-            lower_to_upper(str[i]);
+void capitalize_alpha_chars(char* str, int* strLen, int* count) {
+    for (int i = 0; i < *strLen; i++) {
+        if (lower_case_test(str[i])) {
+            str[i] = lower_to_upper(str[i]);
             (*count)++;
         }
     }
 }
 
-void reverse_string(char* str, int* strLen){
-    int i = 0;
-    int j = *strLen;
+void reverse_string(char* str, int* strLen, char* newStr) {
+    int j = *strLen - 1;
 
-    while(i<j){
-        char temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-        i++;
-        j--;
+    for (int i = 0; i < *strLen; i++, j--) {
+        newStr[i] = str[j];
     }
+    newStr[*strLen] = '\0';
+}
+
+void print(char* newStr, int* strLen, int* count) {
+    for (int i = 0; i < *strLen; i++) {
+        printf("%c\n", newStr[i]);
+    }
+     printf("The string is %d chars and %d chars were capitalized. ", *strLen, *count);
+
 }
