b1a348de7efe952f2caf29c57127d9bc5dc01c50
Gopal Walia <113048741+Gopes02@users.noreply.github.com>
2023-02-25 22:05:11 -0500

    reverse_string function

--- lab4.c
+++ lab4.c
@@ -4,14 +4,18 @@ void string_input(char* str);
 void string_lenght(char* str, int* len);
 int lower_case_test(char c);
 char lower_to_upper(char c);
+void capitalize_alpha_chars(char* str, int* strLen,int* count);
+void reverse_string(char* str, int* strLen);
 #define MAX_LENGHT 100
 
 int main(){
     char str[MAX_LENGHT], lower[MAX_LENGHT];
-    int strLen = 0;
+    int strLen = 0, count = 0;
     string_input(str);
     string_lenght(str, &strLen);
-    
+    capitalize_alpha_chars(str,&strLen,&count);
+    reverse_string(str, &strLen);
+
 }
 
 void string_input(char *str){
@@ -19,7 +23,6 @@ void string_input(char *str){
     printf("Enter a string: ");
     fgets(str,MAX_LENGHT,stdin);
 
-    printf("%s",str);
 }
 
 void string_lenght(char* str, int* strLen){
@@ -28,7 +31,7 @@ void string_lenght(char* str, int* strLen){
         i++;
     }
     *strLen = i-1;
-    printf("%d",*strLen);
+    
 }
 
 int lower_case_test(char c){
@@ -37,4 +40,26 @@ int lower_case_test(char c){
 
 char lower_to_upper(char c){
     return c = c - 'a' + 'A';
+}
+
+void capitalize_alpha_chars(char* str, int* strLen,int* count){
+    for (int i=0;i < *strLen;i++ ){
+        if(lower_case_test(str[i])){
+            lower_to_upper(str[i]);
+            (*count)++;
+        }
+    }
+}
+
+void reverse_string(char* str, int* strLen){
+    int i = 0;
+    int j = *strLen;
+
+    while(i<j){
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
 }
