aad78a0867d04b6477488cdf71d0c93eb191686b
Gopal Walia <113048741+Gopes02@users.noreply.github.com>
2023-03-05 03:28:57 -0500

    two tasks done

--- Strings.c
+++ Strings.c
@@ -2,52 +2,79 @@
 #include <string.h>
 #include <ctype.h>
 
-int isSubstring(char string[], char substring[]) {
-    int string_len = strlen(string);
-    int substring_len = strlen(substring);
-    int i, j;
-    int found = 0;
-    
-    // Convert both strings to lowercase for case-insensitive search
-    for (i = 0; i < string_len; i++) {
-        string[i] = tolower(string[i]);
-    }
-    
-    for (i = 0; i < substring_len; i++) {
-        substring[i] = tolower(substring[i]);
-    }
-    
-    // Iterate over string and check for substring
-    for (i = 0; i < string_len; i++) {
-        found = 1;
-        for (j = 0; j < substring_len; j++) {
-            if (string[i+j] != substring[j]) {
-                found = 0;
-                break;
-            }
-        }
-        if (found) {
-            return 1;
-        }
-    }
-    return 0;
-}
+int charCountsEqual(char str[], char substr[]);
+int isSubstring(char str[], char substr[]);
 
 int main() {
     char string[100];
     char substring[100];
 
     printf("Enter a string: ");
-    scanf("%[^\n]s", string);
+    scanf("%[^\n]", string);
+    getchar();
 
     printf("Enter a substring: ");
-    scanf(" %[^\n]s", substring);
+    scanf("%[^\n]", substring);
+    getchar();
 
-    if (isSubstring(string, substring)) {
-        printf("The substring '%s' is present in the string '%s'.\n", substring, string);
+    if (isSubstring(string, substring))
+        printf("Substring exists in the given string.\n");
+    else
+        printf("Substring does not exist in the given string.\n");
+
+    if (charCountsEqual(string, substring)) {
+        printf("The strings have the same number of character counts.\n");
     } else {
-        printf("The substring '%s' is not present in the string '%s'.\n", substring, string);
+        printf("The strings do not have the same number of character counts.\n");
     }
 
     return 0;
+}
+
+
+int isSubstring(char str[], char substr[]) {
+    int strLen = strlen(str);
+    int substrLen = strlen(substr);
+
+    for (int i = 0; i <= strLen - substrLen; i++) {
+        int j;
+
+        for (j = 0; j < substrLen; j++) {
+            if (tolower(str[i + j]) != tolower(substr[j]))
+                break;
+        }
+
+        if (j == substrLen)
+            return 1;
+    }
+
+    return 0;
+}
+
+
+
+
+
+int charCountsEqual(char str[], char substr[]) {
+    int counts1[26] = {0}, counts2[26] = {0};
+    int i;
+
+    for (i = 0; i < strlen(str); i++) {
+        if (isalpha(str[i])) {
+            counts1[tolower(str[i]) - 'a']++;
+        }
+    }
+    for (i = 0; i < strlen(substr); i++) {
+        if (isalpha(substr[i])) {
+            counts2[tolower(substr[i]) - 'a']++;
+        }
+    }
+
+    for (i = 0; i < 26; i++) {
+        if (counts1[i] != counts2[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
 }
